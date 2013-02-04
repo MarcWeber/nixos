5e6e8060347cd9283175b14e3489b6a26959e792
Marc Weber <marco-oweber@gmx.de>
2013-02-04 08:00:28 +0100

    introduce nicer naming: daemonCfg and poolItemCfg, fix xdebug

--- modules/services/misc/phpfpm.nix
+++ modules/services/misc/phpfpm.nix
@@ -9,27 +9,25 @@ let
   cfg =  config.services.phpfpm;
 
   phpIniFile =
-    {php, phpIniLines, name}:
-      if phpIniLines == null
-      then null # use whatever is default
-      else (pkgs.runCommand name { inherit phpIniLines; }
-        "cat ${php}/etc/php-recommended.ini > $out; echo \"$phpIniLines\" >> $out"
-      );
+    {item, phpIniLines ? "", name}:
+      pkgs.runCommand name { inherit phpIniLines; }
+        "cat ${item.daemonCfg.php}/etc/php-recommended.ini > $out; echo \"$phpIniLines\" >> $out"
+      ;
 
-  preparePool = pool:
+  preparePool = item: # item = item of phpfpm.pools
     let
-      enableXdebug = (pool.xdebug.enable or false);
-      profileDir = pool.xdebug.profileDir or (id: "/tmp/xdebug-profiler-dir-${id}");
+      enableXdebug = item.daemonCfg.xdebug.enable or false;
+      profileDir = item.daemonCfg.xdebug.profileDir or (id: "/tmp/xdebug-profiler-dir-${id}");
       xd = if enableXdebug
         then
-          let remote_host = pool.xdebug.remote_host or "127.0.0.1";
-              remote_port = builtins.toString pool.xdebug.remote_port or 9000;
+          let remote_host = item.xdebug.remote_host or "127.0.0.1";
+              remote_port = builtins.toString item.xdebug.remote_port or 9000;
            in {
              idAppend = "-xdebug";
              phpIniLines = ''
-              zend_extension="${pool.php.xdebug}/lib/xdebug.so"
-              zend_extension_ts="${pool.php.xdebug}/lib/xdebug.so"
-              zend_extension_debug="${pool.php.xdebug}/lib/xdebug.so"
+              zend_extension="${item.daemonCfg.php.xdebug}/lib/xdebug.so"
+              zend_extension_ts="${item.daemonCfg.php.xdebug}/lib/xdebug.so"
+              zend_extension_debug="${item.daemonCfg.php.xdebug}/lib/xdebug.so"
               xdebug.remote_enable=true
               xdebug.remote_host=${remote_host}
               xdebug.remote_port=${remote_port}
@@ -44,48 +42,52 @@ let
         };
       phpIniLines =
           xd.phpIniLines
-          + (pool.phpIniLines or "");
+          + (item.phpIniLines or "");
 
 
       # using phpIniLines create a cfg-id
       iniId = builtins.substring 0 5 (builtins.hash "sha256" (unsafeDiscardStringContext phpIniLines))
               +xd.idAppend;
 
-      phpIni = (pool.phpIniFile or phpIniFile) {
-        inherit (pool) php;
+      phpIni = (item.daemonCfg.phpIniFile or phpIniFile) {
+        inherit item;
         name = "php-${iniId}.ini";
         phpIniLines =
           phpIniLines
-          + optionalString enableXdebug "\nprofiler_output_dir = \"${profileDir iniId}\;";
+          + optionalString enableXdebug "\nprofiler_output_dir = \"${item.daemonCfg.xdebug.profiler_output_dir or (profileDir iniId)}\;";
       };
 
-      id = "${pool.php.id}-${iniId}";
-      hasPhpIni = phpIni == null;
-      phpIniName = if hasPhpIni then baseNameOf (unsafeDiscardStringContext pool.phpIni) else "";
-    in pool // { inherit phpIniName phpIni id; };
+      # [ID] see daemonIdFun
+      id = item.daemonCfg.id or "${item.daemonCfg.php.id}-${iniId}";
+      phpIniName = baseNameOf (unsafeDiscardStringContext item.phpIni);
+    in item // {
+        daemonCfg = item.daemonCfg // {
+          inherit phpIniName phpIni id;
+        };
+      };
 
 
   phpFpmDaemons =
 
     let nv = name: value: listToAttrs [{ inherit name value; }];
         poolsWithIni = map preparePool cfg.pools;
-        poolsByPHP = foldAttrs (n: a: [n] ++ a) [] (map (p: nv "${p.php.id}${p.phpIniName}" p) poolsWithIni);
+        # group pools by common php and php ini config
+        poolsByPHP = foldAttrs (n: a: [n] ++ a) [] (map (p: nv "${p.daemonCfg.id}" p) poolsWithIni);
         toDaemon = name: pools:
             let h = head pools;
-            in h.php.system_fpm_config
+            in h.daemonCfg.php.system_fpm_config
                  { # daemon config
                    # TODO make option or such by letting user set these by php.id attr or such
                    log_level = "notice";
                    emergency_restart_threshold = "10";
                    emergency_restart_interval = "1m";
                    process_control_timeout = "5s";
-                   phpIni =  h.phpIni;
-                   inherit (h) id;
+                   inherit (h.daemonCfg) id phpIni phpIniLines;
                  }
                  # pools
                  (map (p:
                    let socketPath = cfg.socketPathFun p;
-                   in p.pool
+                   in p.poolItemCfg
                       // {
                       listen_address = socketPath;
                       name = builtins.baseNameOf socketPath;
@@ -115,49 +117,63 @@ in {
         description = "Directory where to put in log files.";
       };
 
-      idFun = mkOption {
+      daemonIdFun = mkOption {
         description = "Function returning service name based on php compilation options, php ini file";
-        default = pool: (preparePool pool).id;
+        default = pool: (preparePool pool).daemonCfg.id;
       };
 
       socketPathFun = mkOption {
         description = "Function returning socket path by pool to which web servers connect to.";
         default = pool:
-          let pool_h = builtins.substring 0 8 (builtins.hash "sha256" (builtins.toXML pool.pool));
-          in "/dev/shm/php-fpm-${cfg.idFun pool}-${pool_h}";
+          let pool_h = builtins.substring 0 8 (builtins.hash "sha256" (builtins.toXML pool.poolItemCfg));
+          in "/dev/shm/php-fpm-${cfg.daemonIdFun pool}-${pool_h}";
       };
 
       pools = mkOption {
         default = [];
         example = [
           rec {
-            # php version, must support fpm, thus must have a system_fpm_config attr
-            php = pkgs.php5_2fpm.override {};
 
-            # optional: append addditional php.ini lines.
+            ### php-fpm daemon options: If they differ multiple daemons will be started
+            daemonCfg = {
 
-            # Please note that most options can be set by using etxraLines in
-            # the pool configuration like this:
-            #   php_flag[name]=on/off
-            #   php_admin_value[str_option]="127.0.0.1"
-            # which should be preferred so that less php-fpm daemons have to be started
-            phpIniLines = ''
-            '';
+              ### id
+              # optional:
+              # An ID based on the PHP configuration is generated automatically, see [ID] and daemonIdFun
+              # id = "php-5.3"
+              # this id is used to make the systemd units and the socket paths unique
+              # see daemonIdFun etc.
 
-            # optional: enable xdebug, if set additional phpIniLines will be created
-            # xdebug can't be configured per pool, see: https://bugs.php.net/bug.php?id=54824
-            xdebug = {
-              enable = true;
-              # optional names:
-              # remote_host = "127.0.0.1";
-              # remote_port = 9000;
-              # profiler_output_dir = id: "/tmp/xdebug-profiler-dir-${id}";
+              # php version, must support fpm, thus must have a system_fpm_config attr
+              php = pkgs.php5_2fpm.override {};
+
+              # optional: append addditional php.ini lines.
+
+              # Please note that most options can be set by using etxraLines in
+              # the pool configuration like this:
+              #   php_flag[name]=on/off
+              #   php_admin_value[str_option]="127.0.0.1"
+              # which should be preferred so that less php-fpm daemons have to be started
+              phpIniLines = ''
+              '';
+
+              # optional: enable xdebug, if set additional phpIniLines will be created
+              # xdebug can't be configured per pool, see: https://bugs.php.net/bug.php?id=54824
+              xdebug = {
+                enable = true;
+                # optional names:
+                # remote_host = "127.0.0.1";
+                # remote_port = 9000;
+                # profileDir = id: "/tmp/xdebug-profiler-dir-${id}"; # setting profiler_output_dir
+              };
+
+              # optional: override phpIniFile
+              # phpIni = phpIniFile;
             };
 
-            # optional: override phpIniFile
-            # phpIni = phpIniFile;
+            ### php-fpm per pool options
+            poolItemCfg = {
 
-            pool = {
               # pool config, see system_fpm_config implementation in nixpkgs
               slowlog = ""; # must be writeable by the user/group of the php process?
 
@@ -172,6 +188,7 @@ in {
                 min_spare_servers = 10;
                 max_spare_servers = 30;
               };
+
             };
           }
         ];
