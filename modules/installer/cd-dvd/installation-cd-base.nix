63fa4f9d386ca3acd79bde90a5056618c14f6184
Marc Weber <marco-oweber@gmx.de>
2013-04-23 03:06:59 +0200

    string -> lines, export mountPoint (iso)

--- modules/installer/cd-dvd/installation-cd-base.nix
+++ modules/installer/cd-dvd/installation-cd-base.nix
@@ -63,4 +63,6 @@ in
   # To speed up installation a little bit, include the complete stdenv
   # in the Nix store on the CD.
   isoImage.storeContents = [ pkgs.stdenv pkgs.busybox ];
+
+  environment.shellInit = "export mountPoint=/mnt";
 }
